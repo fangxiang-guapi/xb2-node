40f1ae8f963ba0f27e599f20894d7c0dbde93826
zhongjiang <1487006403@qq.com>
2025-03-07 17:26:29 +0800

    客户端操作

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const express = require('express'); 
 const app = express(); 
 const port = 3000; 
+/** 
+* 使用 JSON 中间件 
+*/ 
+app.use(express.json());
+
 app.listen(port, () => { 
 console.log(' �服务已启动！');
 app.get('/', (request, response) => { 
@@ -25,8 +30,15 @@ app.get('/', (request, response) => {
         ];
 
         app.get('/posts', (request, response) => { 
+            
+            // 获取请求里的数据 
+
+
+
             response.send(data); 
-            });app.get('/posts/:postId', (request, response) => { 
+            });
+            
+            app.get('/posts/:postId', (request, response) => { 
                 console.log(request.params); 
                 // 获取内容 ID 
                 const { postId } = request.params; 
@@ -34,6 +46,28 @@ app.get('/', (request, response) => {
                 const posts = data.filter(item => item.id == postId); 
                 // 作出响应 
                 response.send(posts[0]); 
+
+    
                 });
+                              /** 
+* 创建内容 
+*/ 
+app.post('/posts', (request, response) => { 
+    const { content } = request.body;
+    // 设置响应状态码 
+response.status(201);
+// 输出请求头部数据 
+console.log(request.headers['sing-along']);
+// 设置响应头部数据 
+response.set('Sing-Along', 'How I wonder what you are!');
+                // 作出响应 
+response.send({ 
+    message: `成功创建了内容：${content}` 
+    });
+});
+
+
+
+  
                
 });
